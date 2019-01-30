8e25a339abb2fd16e2a3dc6d65c495de70e52d35
Rajan Sharma <33963570+tacsharma@users.noreply.github.com>
2019-01-30 12:19:46 +0530

    Add files via upload

---
+++ TestExceptionPropagation2.java
@@ -0,0 +1,22 @@
+class TestExceptionPropagation2{  
+  void m(){  
+    throw new java.io.IOException("device error");
+  }  
+  void n(){  
+    m();  
+  }  
+  void p(){  
+   try{  
+    n();  
+   }catch(Exception e){System.out.println("exception handeled");}  
+  }  
+
+  public static void main(String args[]){  
+   TestExceptionPropagation2 obj=new TestExceptionPropagation2();  
+ obj.p();  
+   System.out.println("normal flow");  
+  }  
+} 
+
+
+
